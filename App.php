1c53f2c2afb3de04691096fea4899addd18d43d4
Toni Oriol <tonioriol@gmail.com>
2016-06-05 21:17:17 +0200

    refactored System class

--- App.php
+++ App.php
@@ -1,15 +1,15 @@
 <?php namespace App;
 
-//require_once 'Console.php';
-//require_once 'Output.php';
-//require_once 'System.php';
-//require_once 'Input.php';
-//require_once 'Str.php';
+require_once 'Console.php';
+require_once 'Output.php';
+require_once 'System.php';
+require_once 'Input.php';
+require_once 'Str.php';
 
 
 // check php version, os version and initialize the cli environment
 // if ok continue, else exit(-1) (???)
-//System::instance()->checkPhp()->checkMacos()->initialize();
+System::instance()->check();
 
 // change system languages according to  "--system" option if provided
 // else add catalan on top of any other existing languages
@@ -19,18 +19,14 @@
 
 
 
-Output::write('--' . System::instance()->version() . '--');
+Output::write('System version: ' . System::instance()->version());
 
-//$input = new Input;
+Output::write('Setting system languages to: ' . implode(', ', Input::instance()->options('system')));
+Output::write('defaults write -g AppleLanguages -array ' . implode(', ', Input::instance()->options('system')));
+Console::execute('defaults write -g AppleLanguages -array ' . implode(', ', Input::instance()->options('system')));
 
-//Output::write('Setting system languages to: ' . implode(', ', Input::instance()->options('system')));
-//Output::write('defaults write -g AppleLanguages -array ' . implode(', ', Input::instance()->options('system')));
-//$output = Console::execute('defaults write -g AppleLanguages -array ' . implode(', ', $input->options('system')));
-////Output::write($output);
-
-//Output::write('Setting chrome language to: ' . implode(', ', Input::instance()->options('chrome')));
-//Output::write('defaults write com.google.Chrome AppleLanguages -array ' . implode(', ', Input::instance()->options('chrome')));
-//$output = shell_exec('defaults write com.google.Chrome AppleLanguages -array ' . $input->options('chrome'));
-//Output::write($output);
+Output::write('Setting chrome language to: ' . implode(', ', Input::instance()->options('chrome')));
+Output::write('defaults write com.google.Chrome AppleLanguages -array ' . implode(', ', Input::instance()->options('chrome')));
+Console::execute('defaults write com.google.Chrome AppleLanguages -array ' . implode(', ', Input::instance()->options('chrome')));
 
 exit(0);
--- System.php
+++ System.php
@@ -12,56 +12,75 @@ class System
 
 	public function __construct()
 	{
-		$this->version = $this->getVersion();
+		$this->setVersion();
+	}
+
+	public function setVersion()
+	{
+		$version = Console::execute('sw_vers -productVersion');
+
+		if (preg_match($this->versionNumberPattern, $version)) {
+			$this->version = trim($version);
+		}
+
+		$this->version = null;
 	}
 
 	public function getVersion()
 	{
-		$version = Console::execute('sw_vers -productVersion');
-
-		if(preg_match($this->versionNumberPattern, $version)) {
-			return trim($version);
-		}
-
-		return null;
-	}
-
-	public function version() {
 		return $this->version;
 	}
 
+	public function checkSystem()
+	{
+		return version_compare($this->getVersion(), '', '<=');
+	}
+
 	/**
-	 * Checks if the php runtime is capable of run.
+	 * Checks if the php runtime is capable of run. If so, setup it.
 	 *
 	 * @return $this
 	 */
+	private function checkPhp()
+	{
+		if (version_compare(phpversion(), '4.3.0', '<') || php_sapi_name() == 'cgi') {
+			$this->setupPhp();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Sets up CLI environment based on SAPI and PHP version
+	 */
+	public function setupPhp()
+	{
+		// Handle output buffering
+		@ob_end_flush();
+		ob_implicit_flush(true);
+
+		// PHP ini settings
+		set_time_limit(0);
+		ini_set('track_errors', true);
+		ini_set('html_errors', false);
+		ini_set('magic_quotes_runtime', false);
+
+		// Define stream constants
+		define('STDIN', fopen('php://stdin', 'r'));
+		define('STDOUT', fopen('php://stdout', 'w'));
+		define('STDERR', fopen('php://stderr', 'w'));
+
+		// Close the streams on script termination
+		register_shutdown_function(create_function('', 'fclose(STDIN); fclose(STDOUT); fclose(STDERR); return true;'));
+	}
+
 	public function check()
 	{
-		/**
-		 * Sets up CLI environment based on SAPI and PHP version
-		 */
-		if (version_compare(phpversion(), '4.3.0', '<') || php_sapi_name() == 'cgi') {
-			// Handle output buffering
-			@ob_end_flush();
-			ob_implicit_flush(true);
-
-			// PHP ini settings
-			set_time_limit(0);
-			ini_set('track_errors', true);
-			ini_set('html_errors', false);
-			ini_set('magic_quotes_runtime', false);
-
-			// Define stream constants
-			define('STDIN', fopen('php://stdin', 'r'));
-			define('STDOUT', fopen('php://stdout', 'w'));
-			define('STDERR', fopen('php://stderr', 'w'));
-
-			// Close the streams on script termination
-			register_shutdown_function(create_function('', 'fclose(STDIN); fclose(STDOUT); fclose(STDERR); return true;'));
-
+		if ($this->checkPhp() and $this->checkSystem()) {
 			return $this;
 		} else {
-			exit("PHP version not allowed");
+			exit("System not compatible");
 		}
 	}
 }
